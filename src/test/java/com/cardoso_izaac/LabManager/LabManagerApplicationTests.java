feebfbfe9120b58c0ae63e53b17ad57ac62820a9
Izaac-Cardoso <izaacrcardoso@gmail.com>
2025-06-25 21:09:23 -0300

    first commit

---
+++ src/test/java/com/cardoso_izaac/LabManager/LabManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardoso_izaac.LabManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
